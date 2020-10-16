f86d180722f17bf826e4a83064d7831640a745c2
Dweep <dweep.programmer@gmail.com>
2020-10-16 23:40:02 +0530

    Add: Youtube

--- controllers/usernamecheck.js
+++ controllers/usernamecheck.js
@@ -142,3 +142,26 @@ exports.twitterCheck = (req, res) => {
     return res.status(406).json({ error: "Invalid Username" });
   }
 };
+
+exports.youtubeCheck = (req, res) => {
+  var youtubeRegex = /^[a-zA-Z0-9]*$\w{0,38}/g;
+
+  if (
+    youtubeRegex.test(req.params.username) &&
+    req.params.username.length <= 15
+  ) {
+    fetch("https://www.youtube.com/" + req.params.username)
+      .then((response) => {
+        if (response.status === 404) {
+          return res.json({ success: "Username Available" });
+        } else {
+          return res.json({ error: "Username Taken" });
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  } else {
+    return res.status(406).json({ error: "Invalid Username" });
+  }
+};
